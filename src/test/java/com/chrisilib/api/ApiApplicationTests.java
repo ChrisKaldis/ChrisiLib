d7900aaa2b3afdce41b56abd7692d2166c2856bb
Christos Kaldis <up1059364@ac.upatras.gr>
2025-10-03 14:48:12 +0300

    feat: Initial project structure from Spring Initializr

---
+++ src/test/java/com/chrisilib/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrisilib.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
